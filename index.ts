18c3abc00cfa35a7aa291ece5755df5062c03327
James Barlow <james-b-git@outlook.com>
2023-08-02 21:57:10 -0700

    Save snip as b64

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 import puppeteer from "puppeteer";
 
-await (async () => {
+const start = async (): Promise<void> => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://google.com");
+
+  const snip = await page.screenshot({ encoding: "base64" });
+  console.log(snip.toString());
+
   await browser.close();
-})();
+};
+
+start().catch((e) => {
+  console.error(e);
+});
