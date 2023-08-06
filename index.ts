e77e5317f1be295973be0f0de32022f30e2ca791
James Barlow <james-b-git@outlook.com>
2023-08-06 12:39:12 -0700

    click and keyboard input testing

--- index.ts
+++ index.ts
@@ -5,31 +5,41 @@ const start = async (): Promise<void> => {
     defaultViewport: { width: 1920, height: 1080 },
   });
   const page = await browser.newPage();
-  await page.goto("https://google.com");
+  await page.goto("https://google.com", { waitUntil: "domcontentloaded" });
 
   // Remove scripts
-  await page.evaluate(() => {
-    const el = document.querySelectorAll("script");
-    for (let i = 0; i < el.length; i++) {
-      el[i].parentNode?.removeChild(el[i]);
-    }
-  });
-  const content = await page.content();
+  // await page.evaluate(() => {
+  //   const el = document.querySelectorAll("script");
+  //   for (let i = 0; i < el.length; i++) {
+  //     el[i].parentNode?.removeChild(el[i]);
+  //   }
+  // });
+  // const content = await page.content();
   // console.log(content);
-  await browser.close();
+  // await browser.close();
 
-  const browser2 = await puppeteer.launch({
-    defaultViewport: { width: 1920, height: 1080 },
-  });
-  const page2 = await browser2.newPage();
+  // const browser2 = await puppeteer.launch({
+  //   defaultViewport: { width: 1920, height: 1080 },
+  // });
+  // const page2 = await browser2.newPage();
   // Reload content
-  await page2.setContent(content);
+  // await page2.setContent(content);
+  await page.waitForSelector("#APjFqb", { visible: true });
+  const textarea = await page.$("#APjFqb");
+  await textarea?.click();
+
+  await page.keyboard.type("tester");
+
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: "domcontentloaded" }),
+    page.keyboard.press("Enter"),
+  ]);
 
   // Get screenshot
-  const snip = await page2.screenshot({ encoding: "base64" });
+  const snip = await page.screenshot({ encoding: "base64" });
   console.log(snip);
 
-  await browser2.close();
+  await browser.close();
 };
 
 start().catch((e) => {
