1ba1bdc25b51787d42eb876c38297cd8f8d27853
James Barlow <james-b-git@outlook.com>
2023-08-17 20:50:58 -0700

    Display image diff

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -1,7 +1,10 @@
 import type { V2_MetaFunction } from "@remix-run/node";
-import { map } from "lodash";
+import { groupBy, isNil, map, mapValues, orderBy } from "lodash";
 import { useLoaderData } from "@remix-run/react";
 import { getOutputData } from "~/utils/readData.server";
+import { PNG } from "pngjs";
+import dayjs from "dayjs";
+import Pixelmatch from "pixelmatch";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "Snips" }];
@@ -9,23 +12,61 @@ export const meta: V2_MetaFunction = () => {
 
 export async function loader() {
   const outputs = getOutputData();
-  return outputs;
+  const groupedOutputs = groupBy(outputs, "alias");
+  return mapValues(groupedOutputs, (output) => {
+    const orderedOutputs = orderBy(output, (o) => +dayjs(o.timestamp), "desc");
+    const [mostRecent, secondMostRecent] = orderedOutputs;
+    if (
+      isNil(mostRecent) ||
+      isNil(secondMostRecent) ||
+      mostRecent.snips.length !== secondMostRecent.snips.length
+    ) {
+      return null;
+    }
+
+    const diffs = map(mostRecent.snips, (snip, i) => {
+      const secondSnip = secondMostRecent.snips[i];
+
+      const snipImg1 = PNG.sync.read(Buffer.from(snip.snip, "base64"));
+      const snipImg2 = PNG.sync.read(Buffer.from(secondSnip.snip, "base64"));
+
+      const { width, height } = snipImg1;
+
+      const diff = new PNG({ width, height });
+      const numDifferentPixels = Pixelmatch(
+        snipImg1.data,
+        snipImg2.data,
+        diff.data,
+        width,
+        height,
+      );
+
+      return {
+        numDifferentPixels,
+        diffImg: PNG.sync.write(diff).toString("base64"),
+      };
+    });
+    return { diffs, snipFlows: orderedOutputs };
+  });
 }
 
 export default function Index() {
   const allOutputs = useLoaderData<typeof loader>();
-  console.log(allOutputs);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      {map(allOutputs, (output) => {
+      {map(allOutputs, (output, alias) => {
         return (
-          <div key={output.timestamp}>
-            <div>{output.timestamp}</div>
-            {map(output.snips, (snip) => (
-              <img
-                src={`data:image/png;base64, ${snip.snip}`}
-                style={{ maxWidth: "100%" }}
-              />
+          <div>
+            <h1>{alias}</h1>
+            <h2>Diffs</h2>
+            {map(output?.diffs, (o) => (
+              <div>
+                <div>Pixel difference: {o.numDifferentPixels}</div>
+                <img
+                  src={`data:image/png;base64, ${o.diffImg}`}
+                  style={{ maxWidth: "100%" }}
+                />
+              </div>
             ))}
           </div>
         );
