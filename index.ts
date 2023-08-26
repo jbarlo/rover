64bb8cedadc2d0a1b626a12ecf4cee41d0ed06e5
James Barlow <james-b-git@outlook.com>
2023-08-25 19:31:24 -0700

    Run multiple configs

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import puppeteer, { type Page } from "puppeteer";
 import dayjs from "dayjs";
 import crypto from "crypto";
 import fs from "fs";
-import { isNil } from "lodash";
+import { isNil, map, forEach } from "lodash";
 import type { StepWithExtras, Step } from "./types";
 import { getStepConfigs } from "./utils";
 
@@ -39,83 +39,84 @@ const getSnip = async (page: Page, step: StepWithExtras): Promise<string> => {
   return (await page.screenshot({ encoding: "base64" })).toString();
 };
 
-const start = async (): Promise<void> => {
-  // TODO multi-config run
-  const config = getStepConfigs(".")?.[0]?.config;
-  if (isNil(config)) return;
+const start = () => {
+  const configs = map(getStepConfigs("."), (c) => ({
+    ...c.config,
+    alias: isNil(c.config.alias) ? c.filename : c.config.alias,
+  }));
 
-  const browser = await puppeteer.launch({
-    defaultViewport: { width: 1920, height: 1080 },
-  });
-  const page = await browser.newPage();
-  await page.goto(config.url, { waitUntil: "domcontentloaded" });
+  forEach(configs, async (config) => {
+    const browser = await puppeteer.launch({
+      defaultViewport: { width: 1920, height: 1080 },
+    });
+    const page = await browser.newPage();
+    await page.goto(config.url, { waitUntil: "domcontentloaded" });
 
-  // Remove scripts
-  // await page.evaluate(() => {
-  //   const el = document.querySelectorAll("script");
-  //   for (let i = 0; i < el.length; i++) {
-  //     el[i].parentNode?.removeChild(el[i]);
-  //   }
-  // });
-  // const content = await page.content();
-  // console.log(content);
-  // await browser.close();
+    // Remove scripts
+    // await page.evaluate(() => {
+    //   const el = document.querySelectorAll("script");
+    //   for (let i = 0; i < el.length; i++) {
+    //     el[i].parentNode?.removeChild(el[i]);
+    //   }
+    // });
+    // const content = await page.content();
+    // console.log(content);
+    // await browser.close();
 
-  // const browser2 = await puppeteer.launch({
-  //   defaultViewport: { width: 1920, height: 1080 },
-  // });
-  // const page2 = await browser2.newPage();
-  // Reload content
-  // await page2.setContent(content);
+    // const browser2 = await puppeteer.launch({
+    //   defaultViewport: { width: 1920, height: 1080 },
+    // });
+    // const page2 = await browser2.newPage();
+    // Reload content
+    // await page2.setContent(content);
 
-  const snips: Snip[] = [];
+    const snips: Snip[] = [];
 
-  await config.steps.reduce(
-    (promise: Promise<void>, step: StepWithExtras, i: number) =>
-      promise.then(async () => {
-        await runStep(page, step);
+    await config.steps.reduce(
+      (promise: Promise<void>, step: StepWithExtras, i: number) =>
+        promise.then(async () => {
+          await runStep(page, step);
+          // Get screenshot
+          if (step.screenShotParams?.noSnip !== true) {
+            const snip = await getSnip(page, step);
+            snips.push({ index: i, snip });
+          }
+        }),
+      (async () => {
         // Get screenshot
-        if (step.screenShotParams?.noSnip !== true) {
-          const snip = await getSnip(page, step);
-          snips.push({ index: i, snip });
-        }
-      }),
-    (async () => {
-      // Get screenshot
-      const snip = await page.screenshot({ encoding: "base64" });
-      snips.push({ index: -1, snip });
-    })(),
-  );
+        const snip = await page.screenshot({ encoding: "base64" });
+        snips.push({ index: -1, snip });
+      })(),
+    );
 
-  const now = dayjs().toISOString();
+    const now = dayjs().toISOString();
 
-  const stepsHash = crypto
-    .createHash("md5")
-    .update(JSON.stringify(config.steps))
-    .digest("hex");
+    const stepsHash = crypto
+      .createHash("md5")
+      .update(JSON.stringify(config.steps))
+      .digest("hex");
 
-  const dir = `./output/${config.alias}-${stepsHash}`;
+    const dir = `./output/${config.alias}-${stepsHash}`;
 
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
-  }
-  fs.writeFileSync(`${dir}/${now}.json`, JSON.stringify(snips));
-  // const heapSnapshot = await captureHeapSnapshot(page.target());
-  // console.log(
-  //   JSON.stringify(
-  //     findObjectsWithProperties(
-  //       heapSnapshot,
-  //       ["name", "price", "description", "sku"],
-  //       { ignoreProperties: ["nutritionProfiles", "image", "categories"] },
-  //     ),
-  //     undefined,
-  //     4,
-  //   ),
-  // );
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    fs.writeFileSync(`${dir}/${now}.json`, JSON.stringify(snips));
+    // const heapSnapshot = await captureHeapSnapshot(page.target());
+    // console.log(
+    //   JSON.stringify(
+    //     findObjectsWithProperties(
+    //       heapSnapshot,
+    //       ["name", "price", "description", "sku"],
+    //       { ignoreProperties: ["nutritionProfiles", "image", "categories"] },
+    //     ),
+    //     undefined,
+    //     4,
+    //   ),
+    // );
 
-  await browser.close();
+    await browser.close();
+  });
 };
 
-start().catch((e) => {
-  console.error(e);
-});
+start();
