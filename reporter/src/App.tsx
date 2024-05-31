854d0340305578b08f29f2d31b9e416eb7aefdd4
James Barlow <james-b-git@outlook.com>
2024-05-30 20:18:35 -0700

    Init reporter

--- reporter/src/App.tsx
+++ reporter/src/App.tsx
@@ -1,5 +1,17 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="reporter-ui-theme">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Hello world!
+      </h1>
+      <div>
+        <Button variant="default">tester</Button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- reporter/vite.config.ts
+++ reporter/vite.config.ts
@@ -1,8 +1,20 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 });
