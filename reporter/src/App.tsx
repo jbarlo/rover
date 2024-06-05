358858c911a86216a383728dc007669d7b4fe98c
James Barlow <james-b-git@outlook.com>
2024-06-04 21:22:23 -0700

    Implement file list sider

--- reporter/src/App.tsx
+++ reporter/src/App.tsx
@@ -1,19 +1,46 @@
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/themeProvider";
 import Importer from "./components/importer";
+import FileList from "./components/fileList";
+import { useState } from "react";
+import { map, slice } from "lodash";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "./components/ui/resizable";
 
 function App() {
+  const [files, setFiles] = useState<File[]>([]);
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="reporter-ui-theme">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Hello world!
-      </h1>
-      <div>
-        <Importer
-          onImport={(files) => {
-            console.log(files);
-          }}
-        />
-      </div>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel className="p-2" defaultSize={25}>
+          <div>
+            <Importer
+              onImport={(f) => {
+                setFiles((prev) => [...prev, ...f]);
+              }}
+            />
+          </div>
+          <FileList
+            files={map(files, (f, i) => ({
+              id: `${i}-${f.name}`,
+              index: i,
+              name: f.name,
+            }))}
+            onClick={() => {}}
+            onClickRemove={(f) => {
+              setFiles((prev) => [
+                ...slice(prev, 0, f.index),
+                ...slice(prev, f.index + 1),
+              ]);
+            }}
+          />
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel className="p-2">TODO</ResizablePanel>
+      </ResizablePanelGroup>
     </ThemeProvider>
   );
 }
---
+++ reporter/src/components/fileList.tsx
@@ -0,0 +1,49 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Cross1Icon } from "@radix-ui/react-icons";
+import { map } from "lodash";
+import { Button } from "./ui/button";
+
+export interface FileListProps<FileData extends { id: string; name: string }> {
+  files: FileData[];
+  onClick?: (file: FileData) => void;
+  onClickRemove?: (file: FileData) => void;
+}
+
+const FileList = <FileData extends { id: string; name: string }>({
+  files,
+  onClick,
+  onClickRemove,
+}: FileListProps<FileData>) => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableHead>Reports</TableHead>
+      </TableHeader>
+      <TableBody>
+        {map(files, (file) => (
+          <TableRow key={file.name} onClick={() => onClick?.(file)}>
+            <TableCell>{file.name}</TableCell>
+            <TableCell>
+              <Button
+                variant="destructive"
+                onClick={() => onClickRemove?.(file)}
+              >
+                <Cross1Icon className="mr-2" />
+                Remove
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default FileList;
