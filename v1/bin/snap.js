cbba81698d978723c1aac0ad04df2472494018ad
James Barlow <james-b-git@outlook.com>
2024-06-25 18:04:27 -0700

    Remove old versions

--- v1/bin/snap.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-const shell = require("shelljs");
-const path = require("path");
-
-const distSnapPath = path.resolve(__dirname, "../dist/snap/index.js");
-shell.exec(`node ${distSnapPath}`);
