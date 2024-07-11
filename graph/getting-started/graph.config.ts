dea81013cba1d4729b9b97066b914e18015e3903
James Barlow <james-b-git@outlook.com>
2024-07-10 23:35:01 -0700

    Slap together a minimal getting started

---
+++ graph/getting-started/graph.config.ts
@@ -0,0 +1,35 @@
+// TODO nonrelative import
+import { configure } from "../src/configuration.js";
+
+export default configure({
+  beforeEach: async () => {},
+  afterEach: async () => {},
+  beforeAll: async () => {},
+  afterAll: async () => {},
+  graph: {
+    states: [{ id: "a", url: "localhost:3000" }, { id: "b" }, { id: "c" }],
+    edges: [
+      {
+        from: "a",
+        to: "b",
+        name: "a->b",
+        action: async () => {},
+      },
+      {
+        from: "b",
+        to: "c",
+        name: "b->c",
+        resourceEffects: { thingies: 1 },
+        action: async () => {},
+      },
+      {
+        from: "c",
+        to: "a",
+        name: "c->a",
+        resourceEffects: { thingies: -1 },
+        action: async () => {},
+      },
+    ],
+    resources: ["thingies"],
+  },
+});
